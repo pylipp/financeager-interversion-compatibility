d18c865fa854753377bc3e30cb36077af2e5be9a
pylipp <philipp.metzner@tum.de>
2019-09-01 22:55:15 +0200

    Activate venv instead of using venv interpreter
    Avoid errors with interpreter path.

--- run.sh
+++ run.sh
@@ -5,32 +5,35 @@ set -e
 # Check out appropriate backend version
 cd backend
 git checkout "$FINANCEAGER_BACKEND"
+source .venv/bin/activate
 
 # Output version info
-.venv/bin/python -c "
+python -c "
 from financeager import __version__
 print(__version__)"
 
 # Start webservice
-.venv/bin/python -c "
+python -c "
 from financeager import fflask
 fflask.create_app().run()" &
 WEBSERVICE_PID=$!
 sleep 2
+deactivate
 cd ..
 
 # Check out appropriate frontend version
 cd frontend
 git checkout "$FINANCEAGER_FRONTEND"
+source .venv/bin/activate
 
 # Output version info
-.venv/bin/python -c "
+python -c "
 from financeager import __version__
 print(__version__)"
 
 # Run test
 cd test
-.venv/bin/python -c "
+python -c "
 import unittest
 import sys
 from test_cli import CliFlaskTestCase, suite
@@ -41,6 +44,7 @@ test_runner = unittest.TextTestRunner(verbosity=2)
 result = test_runner.run(suite())
 sys.exit(0 if result.wasSuccessful() else 1)
 "
+deactivate
 
 # Terminate webservice
 kill $WEBSERVICE_PID
