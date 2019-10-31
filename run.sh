c51a5985e56b99032e9b516617a02591c0370443
pylipp <philipp.metzner@tum.de>
2019-10-31 08:22:24 +0100

    Reduce command output for debugging

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
-
 VERSIONS=( v0.22 v0.21 )
 
 for FINANCEAGER_BACKEND in "${VERSIONS[@]}"; do
     for FINANCEAGER_FRONTEND in "${VERSIONS[@]}"; do
         [[ $FINANCEAGER_BACKEND = $FINANCEAGER_FRONTEND ]] && continue
 
+        echo $FINANCEAGER_BACKEND $FINANCEAGER_FRONTEND
         echo $FINANCEAGER_BACKEND $FINANCEAGER_FRONTEND >> result.txt
 
         # Check out appropriate backend version
-        ( cd backend && git checkout "$FINANCEAGER_BACKEND" )
+        ( cd backend && git checkout -q "$FINANCEAGER_BACKEND" )
         source backend/.venv/bin/activate
-        pip install -U ./backend
+        pip install -qqq -U ./backend
 
         # Start webservice
         python/print_financeager_version
@@ -23,9 +22,9 @@ for FINANCEAGER_BACKEND in "${VERSIONS[@]}"; do
         deactivate
 
         # Check out appropriate frontend version
-        (cd frontend && git checkout "$FINANCEAGER_FRONTEND")
+        (cd frontend && git checkout -q "$FINANCEAGER_FRONTEND")
         source frontend/.venv/bin/activate
-        pip install -U ./frontend
+        pip install -qqq -U ./frontend
 
         # Run test
         python/print_financeager_version
