44f0e9920d0d0fdbaf4b61721fdcad75ab497672
pylipp <philipp.metzner@tum.de>
2019-10-31 08:09:40 +0100

    Run interversion tests sequentially
    - cannot create several Flask apps at same port (consider extending
    create_app()) but travis jobs run in parallel
    - travis stages run sequentially but only proceed when stage succeeds
    - as workaround, iterate over combinations of versions and run test

--- run.sh
+++ run.sh
@@ -2,27 +2,38 @@
 
 set -e
 
-# Check out appropriate backend version
-( cd backend && git checkout "$FINANCEAGER_BACKEND" )
-source backend/.venv/bin/activate
-pip install -U ./backend
+VERSIONS=( v0.22 v0.21 )
 
-# Start webservice
-python/print_financeager_version
-python/start_webservice &
-WEBSERVICE_PID=$!
-sleep 2
-deactivate
+for FINANCEAGER_BACKEND in "${VERSIONS[@]}"; do
+    for FINANCEAGER_FRONTEND in "${VERSIONS[@]}"; do
+        [[ $FINANCEAGER_BACKEND = $FINANCEAGER_FRONTEND ]] && continue
 
-# Check out appropriate frontend version
-(cd frontend && git checkout "$FINANCEAGER_FRONTEND")
-source frontend/.venv/bin/activate
-pip install -U ./frontend
+        echo $FINANCEAGER_BACKEND $FINANCEAGER_FRONTEND >> result.txt
 
-# Run test
-python/print_financeager_version
-PYTHONPATH=frontend/test python/run_test
-deactivate
+        # Check out appropriate backend version
+        ( cd backend && git checkout "$FINANCEAGER_BACKEND" )
+        source backend/.venv/bin/activate
+        pip install -U ./backend
 
-# Terminate webservice
-kill $WEBSERVICE_PID
+        # Start webservice
+        python/print_financeager_version
+        python/start_webservice &
+        WEBSERVICE_PID=$!
+        sleep 2
+        deactivate
+
+        # Check out appropriate frontend version
+        (cd frontend && git checkout "$FINANCEAGER_FRONTEND")
+        source frontend/.venv/bin/activate
+        pip install -U ./frontend
+
+        # Run test
+        python/print_financeager_version
+        PYTHONPATH=frontend/test python/run_test &&\
+            sed -i '$ s/$/ compatible/' result.txt
+        deactivate
+
+        # Terminate webservice
+        kill $WEBSERVICE_PID
+    done
+done
